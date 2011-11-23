0db60735e0d4466fa1c7f02ae18704cdd24fef5a
Ben Hoskings <ben@hoskings.net>
2011-11-23 18:04:36 +1100

    Prototype.

---
+++ omglog.rb
@@ -0,0 +1,16 @@
+require 'rb-fsevent'
+
+LOG_CMD = 'git log --all --graph --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
+CLEAR = "\e[2J"
+
+abort("Run with a single argument (the directory to omglog).") unless ARGV.length == 1
+
+FSEvent.new.tap {|fsevent|
+  fsevent.watch(File.join(ARGV[0], '.git')) {|directories|
+    rows = (`tput lines`.to_i * 0.7).floor
+    `#{LOG_CMD} -#{rows}`.tap {|log|
+      STDOUT.puts CLEAR + log
+    }
+  }
+  fsevent.run
+}
