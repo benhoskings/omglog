151f402bf017218a2e52304b3222e90192eecfdd
Ben Hoskings <ben@hoskings.net>
2011-11-23 19:17:53 +1100

    Put message last, since it's the truncateable one.

--- omglog.rb
+++ omglog.rb
@@ -1,14 +1,14 @@
 require 'rb-fsevent'
 
-LOG_CMD = %{git log --all --graph --pretty="format:%h%d \2%s\3 \2%an,\3 \2%ar\3"}
+LOG_CMD = %{git log --all --graph --pretty="format:%h%d \2%an,\3 \2%ar\3 \2%s\3"}
 
 LOG_REGEX = %r{
     ([*|/\\_\-.\s]+) # graph
  \s ([\w]{7,40})     # ref
 (\s \(.*\))?         # decorations
- \s \u0002(.*)\u0003 # message
  \s \u0002(.*)\u0003 # author
  \s \u0002(.*)\u0003 # timestamp
+ \s \u0002(.*)\u0003 # message
 }x
 
 CLEAR = "\e[2J"
