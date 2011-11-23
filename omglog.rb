dc63cb8cc6e55b7b1ef0181e5e0ab306bd55b10f
Ben Hoskings <ben@hoskings.net>
2011-11-23 19:13:19 +1100

    Example `git log` output.

--- omglog.rb
+++ omglog.rb
@@ -13,6 +13,9 @@ LOG_REGEX = %r{
 
 CLEAR = "\e[2J"
 
+# example `git log` output
+# "*   \e[33m7c3240d\e[34m (HEAD, origin/master, origin/HEAD, master)\e[m Merge branch 'versions' \e[37m Ben Hoskings, 11 minutes ago\e[m"
+# "*   7c3240d  (HEAD, origin/master, origin/HEAD, master) 'Merge branch 'versions'' 'Ben Hoskings' '16 minutes ago'"
 
 def omglog
   rows = (`tput lines`.to_i * 0.7).floor
