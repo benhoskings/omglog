85d2f3f6a3917489ce514ab579d54214688368aa
Ben Hoskings <ben@hoskings.net>
2011-11-23 18:51:32 +1100

    Parseable git formatting with a corresponding regexp.

--- omglog.rb
+++ omglog.rb
@@ -1,6 +1,16 @@
 require 'rb-fsevent'
 
-LOG_CMD = 'git log --all --graph --pretty="format:%C(yellow)%h%Cblue%d%Creset %s %C(white) %an, %ar%Creset"'
+LOG_CMD = %{git log --all --graph --pretty="format:%h%d \2%s\3 \2%an,\3 \2%ar\3"}
+
+LOG_REGEX = %r{
+    ([*|/\\_\-.\s]+) # graph
+ \s ([\w]{7,40})     # ref
+(\s \(.*\))?         # decorations
+ \s \u0002(.*)\u0003 # message
+ \s \u0002(.*)\u0003 # author
+ \s \u0002(.*)\u0003 # timestamp
+}x
+
 CLEAR = "\e[2J"
 
 abort("Run with a single argument (the directory to omglog).") unless ARGV.length == 1
