5605d492b80b595f7d5535af5960bb5e2ef57ed2
Ben Hoskings <ben@hoskings.net>
2021-03-21 17:16:31 +1100

    No need for #tap here.

--- lib/linux.rb
+++ lib/linux.rb
@@ -3,16 +3,15 @@ require 'rb-inotify'
 module Omglog
   class Linux
     def self.on_change &block
-      INotify::Notifier.new.tap do |notifier|
-        notifier.watch('.git', :modify, :recursive, &block)
-        ['TERM', 'INT', 'QUIT'].each do |signal|
-          trap(signal) do
-            notifier.stop
-            exit(0)
-          end
+      notifier = INotify::Notifier.new
+      notifier.watch('.git', :modify, :recursive, &block)
+      ['TERM', 'INT', 'QUIT'].each do |signal|
+        trap(signal) do
+          notifier.stop
+          exit(0)
         end
-        notifier.run
       end
+      notifier.run
     end
   end
 end
--- lib/osx.rb
+++ lib/osx.rb
@@ -3,10 +3,9 @@ require 'rb-fsevent'
 module Omglog
   class OSX
     def self.on_change &block
-      FSEvent.new.tap {|fsevent|
-        fsevent.watch('.git', &block)
-        fsevent.run
-      }
+      fsevent = FSEvent.new
+      fsevent.watch('.git', &block)
+      fsevent.run
     end
   end
 end
